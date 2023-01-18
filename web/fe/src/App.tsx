54af99e8114ab0276e038ed524d96ce1ae731793
SysDevGit <x@example.com>
2023-01-18 21:44:40 +0100

    First FE commit

---
+++ web/fe/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        FE APP TBD
+      </header>
+    </div>
+  );
+}
+
+export default App;
